4156d963ad456677350e9dfa1e4af107b512e4b0
lordgareth10 <51813599+lordgareth10@users.noreply.github.com>
2026-06-01 03:25:11 +0800

    Revert "Merge pull request #1 from nussucommcell/updated-changes"
    This reverts commit 6625a5a1f9ce19b96869ee384f82b0d073f05d30, reversing
    changes made to 32895aa7c76f154b55072314b21fdd0b5e893d9a.

--- assets/framerusercontent.com/sites/2IhlajlvUOVx0n57b8Ivhv/siteMetadata.BawicdWI.mjs
+++
@@ -1,8 +0,0 @@
-import{t as e}from"./rolldown-runtime.BXVgRTY5.mjs";function t(e,t){return{customHTMLBodyStart:`<style>
-::-webkit-scrollbar {
-    display: none;
-}
-</style>`,customHTMLHeadStart:`<script async="" src=(location.origin+"/assets/pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2504205396698437") crossorigin="anonymous"><\/script>
-
-`,description:`The National University of Singapore Students’ Union (NUSSU) is a unique, student-run organization operating autonomously from the university administration in terms of operations and budget.`,favicon:(location.origin+"/assets/framerusercontent.com/assets/YcXlLszyuz7DSyLYTY82QGRYaM.png"),robots:`max-image-preview:large`,socialImage:(location.origin+"/assets/framerusercontent.com/assets/PjJWyXEOzofwiaTdp31lFa4UIw.png"),title:`NUS Students' Union (NUSSU) Offical Website`}}var n=e((()=>{}));export{t as n,n as t};
-//# sourceMappingURL=siteMetadata.BawicdWI.mjs.map
